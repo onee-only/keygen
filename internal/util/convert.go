a971f869f33d8541517374cc70b6a008a4bfe339
onee-only <kimww0306@gmail.com>
2023-11-15 13:41:19 +0900

    Rename convert package to util

--- internal/util/convert.go
+++ internal/util/convert.go
@@ -1,7 +1,11 @@
-package convert
+package util
 
 import "unsafe"
 
 func StrToBytes(s string) []byte {
 	return unsafe.Slice(unsafe.StringData(s), len(s))
+}
+
+func BytesToStr(b []byte) string {
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
